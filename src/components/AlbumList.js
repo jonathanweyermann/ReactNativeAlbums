3c6bb46cccd3560003348e57d9b40a6dac44af45
jonathan <weyermannx@gmail.com>
2018-03-31 14:16:55 -0600

    First commit. Cards and CardSection built

---
+++ src/components/AlbumList.js
@@ -0,0 +1,36 @@
+// Import libraries for making a component
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
+
+// Make a component
+class AlbumList extends Component {
+  state = { albums: [] };
+
+
+  componentWillMount() {
+    axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+      .then(response => this.setState({ albums: response.data }));
+      console.log('componentWillMount is AlbumList');
+  }
+
+  renderAlbums() {
+    return this.state.albums.map(album =>
+      <AlbumDetail album={album} key={album.title}/>
+    );
+  }
+
+  render() {
+    console.log(this.state);
+
+    return (
+        <View>
+          {this.renderAlbums()}
+        </View>
+    );
+  }
+}
+
+// Make the component available to other parts of the app
+export default AlbumList;
